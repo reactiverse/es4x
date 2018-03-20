9a283d89bb9611724061ee8bb5afe5c0ef760575
Paulo Lopes <paulo@mlopes.net>
2018-03-20 15:56:48 +0100

    move all resouces under the project package and add copyright headers

--- src/main/java/io/reactiverse/es4x/nashorn/Loader.java
+++ src/main/java/io/reactiverse/es4x/nashorn/Loader.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2018 Red Hat, Inc.
+ *
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License v1.0
+ *  and Apache License v2.0 which accompanies this distribution.
+ *
+ *  The Eclipse Public License is available at
+ *  http://www.eclipse.org/legal/epl-v10.html
+ *
+ *  The Apache License v2.0 is available at
+ *  http://www.opensource.org/licenses/apache2.0.php
+ *
+ *  You may elect to redistribute this code under either of these licenses.
+ */
 package io.reactiverse.es4x.nashorn;
 
 import io.reactiverse.es4x.nashorn.runtime.VertxRuntime;
@@ -36,9 +51,9 @@ class Loader {
     VertxRuntime.install(globalBindings);
 
     // install the commonjs loader
-    engine.invokeFunction("load", "classpath:jvm-npm.js");
-    // add support for promises
-    engine.invokeFunction("load", "classpath:promise.js");
+    engine.invokeFunction("load", "classpath:io/reactiverse/es4x/jvm-npm.js");
+    // add polyfills
+    engine.invokeFunction("load", "classpath:io/reactiverse/es4x/polyfill.js");
   }
 
   ScriptEngine getEngine() {
