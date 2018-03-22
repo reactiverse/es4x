c3f802f7b044e293160d52be08bf5a6e0aca49a4
Paulo Lopes <paulo@mlopes.net>
2018-03-22 10:46:13 +0100

    Add basic example

---
+++ examples/empty-project/index.test.js
@@ -0,0 +1,10 @@
+import { TestSuite } from '@vertx/unit';
+
+const suite = TestSuite.create("the_test_suite");
+
+suite.test("my_test_case", function (context) {
+  var s = "value";
+  context.assertEquals("value", s);
+});
+
+suite.run();
