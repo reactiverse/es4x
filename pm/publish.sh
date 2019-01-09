788636322f89477f86eb4ddde9cb07c8fb1de87c
Paulo Lopes <paulo@mlopes.net>
2019-01-09 15:57:36 +0100

    helper to deploy to npm

---
+++ pm/publish.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+mvn clean package
+cd target
+mkdir package
+VERSION=$(cat classes/META-INF/es4x-commands/VERSIONS.properties | grep es4x | cut -c6-)
+tar -zxvf es4x-pm-${VERSION}-bin.tar.gz --strip-components=1 -C package
+
+echo "
+{
+  \"name\": \"es4x-pm\",
+  \"version\": \"${VERSION}\",
+  \"description\": \"ES4X Project Manager Utilities\",
+  \"scripts\": {
+    \"test\": \"echo \\\"Error: no test specified\\\" && exit 1\"
+  },
+  \"author\": \"Paulo Lopes <pmlopes@gmail.com>\",
+  \"license\": \"MIT\",
+  \"bin\": {
+    \"es4x\": \"./bin/es4x\",
+    \"es4x-pm\": \"./bin/es4x-pm\"
+  }
+}
+" > package/package.json
+
+cd package
+npm publish --registry http://localhost:4873
