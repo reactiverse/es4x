f66a1416643ce412e000572af55b7e7ab0b6339f
Paulo Lopes <pmlopes@gmail.com>
2019-05-10 12:26:44 +0200

    Add missing docs/headers

--- es4x/src/main/java/io/reactiverse/es4x/EventEmitter.java
+++ es4x/src/main/java/io/reactiverse/es4x/EventEmitter.java
@@ -1,8 +1,37 @@
+/*
+ * Copyright 2018 Red Hat, Inc.
+ *
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License v1.0
+ *  and Apache License v2.0 which accompanies this distribution.
+ *
+ *  The Eclipse Public License is available at
+ *  http://www.eclipse.org/legal/epl-v10.html
+ *
+ *  The Apache License v2.0 is available at
+ *  http://www.opensource.org/licenses/apache2.0.php
+ *
+ *  You may elect to redistribute this code under either of these licenses.
+ */
 package io.reactiverse.es4x;
 
+/**
+ * Simple EventEmitter
+ */
 public interface EventEmitter {
 
+  /**
+   * Register a callback for an event.
+   *
+   * @param eventName the event name
+   * @param callback the action to perform
+   */
   void on(String eventName, Runnable callback);
 
+  /**
+   * Will trigger the event callback if any on the given event.
+   *
+   * @param eventName the event.
+   */
   void emit(String eventName);
 }
