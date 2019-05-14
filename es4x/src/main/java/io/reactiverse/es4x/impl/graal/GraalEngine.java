3e80f4268c8328b91fd3ca8fc3b5195981e9ac6c
Paulo Lopes <pmlopes@gmail.com>
2019-05-14 12:12:34 +0200

    bump graal to stable

--- es4x/src/main/java/io/reactiverse/es4x/impl/graal/GraalEngine.java
+++ es4x/src/main/java/io/reactiverse/es4x/impl/graal/GraalEngine.java
@@ -68,8 +68,7 @@ public class GraalEngine implements ECMAEngine {
       .allowPublicAccess(true)
       .allowArrayAccess(true)
       .allowListAccess(true)
-      // TODO: enable once graal 19 SDK is on maven
-      //.allowAllImplementations(true)
+      .allowAllImplementations(true)
       // map native JSON Object to Vert.x JSONObject
       .targetTypeMapping(
         Value.class,
