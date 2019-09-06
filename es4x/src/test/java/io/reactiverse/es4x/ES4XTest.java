35446c5c923f7e4da00de5e7911ab2563a4c25c3
Paulo Lopes <pmlopes@gmail.com>
2019-09-06 20:28:00 +0200

    cleanups

--- es4x/src/test/java/io/reactiverse/es4x/ES4XTest.java
+++ es4x/src/test/java/io/reactiverse/es4x/ES4XTest.java
@@ -17,6 +17,10 @@ public class ES4XTest {
   public void cleanUp() {
     System.clearProperty("prefix");
     System.clearProperty("polyglot");
+    System.clearProperty("inspect");
+    System.clearProperty("es4x.prefix");
+    System.clearProperty("es4x.polyglot");
+    System.clearProperty("polyglot.inspect");
   }
 
   @Test
@@ -73,5 +77,49 @@ public class ES4XTest {
     assertNull(System.getProperty("polyglot"));
     assertNotNull(System.getProperty("es4x.polyglot"));
     assertEquals("false", System.getProperty("es4x.polyglot"));
+  }
+
+  @Test
+  public void testInspect() {
+    System.setProperty("inspect", "");
+    ES4X runner = new ES4X();
+    runner.beforeStartingVertx(new VertxOptions());
+    assertNull(System.getProperty("inspect"));
+    assertNotNull(System.getProperty("polyglot.inspect"));
+    assertEquals("9229", System.getProperty("polyglot.inspect"));
+  }
+
+  @Test
+  public void testInspectBrk() {
+    System.setProperty("inspect-brk", "");
+    ES4X runner = new ES4X();
+    runner.beforeStartingVertx(new VertxOptions());
+    assertNull(System.getProperty("inspect-brk"));
+    assertNotNull(System.getProperty("polyglot.inspect"));
+    assertNotNull(System.getProperty("polyglot.inspect.Suspend"));
+    assertEquals("9229", System.getProperty("polyglot.inspect"));
+    assertEquals("true", System.getProperty("polyglot.inspect.Suspend"));
+  }
+
+  @Test
+  public void testInspectWithValue() {
+    System.setProperty("inspect", "11111");
+    ES4X runner = new ES4X();
+    runner.beforeStartingVertx(new VertxOptions());
+    assertNull(System.getProperty("inspect"));
+    assertNotNull(System.getProperty("polyglot.inspect"));
+    assertEquals("11111", System.getProperty("polyglot.inspect"));
+  }
+
+  @Test
+  public void testInspectBrkWithValue() {
+    System.setProperty("inspect-brk", "11223");
+    ES4X runner = new ES4X();
+    runner.beforeStartingVertx(new VertxOptions());
+    assertNull(System.getProperty("inspect-brk"));
+    assertNotNull(System.getProperty("polyglot.inspect"));
+    assertNotNull(System.getProperty("polyglot.inspect.Suspend"));
+    assertEquals("11223", System.getProperty("polyglot.inspect"));
+    assertEquals("true", System.getProperty("polyglot.inspect.Suspend"));
   }
 }
