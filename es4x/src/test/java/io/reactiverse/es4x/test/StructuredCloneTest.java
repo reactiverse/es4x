f5193692b259874494ecd6c6b6fed392f5678323
Paulo Lopes <pmlopes@gmail.com>
2020-04-24 11:54:36 +0200

    Replace current complex console formatter with a logger, levels can be enabled or disabled at runtime
    Signed-off-by: Paulo Lopes <pmlopes@gmail.com>

---
+++ es4x/src/test/java/io/reactiverse/es4x/test/StructuredCloneTest.java
@@ -0,0 +1,39 @@
+package io.reactiverse.es4x.test;
+
+import io.reactiverse.es4x.Runtime;
+import io.vertx.ext.unit.Async;
+import io.vertx.ext.unit.TestContext;
+import io.vertx.ext.unit.junit.RunTestOnContext;
+import io.vertx.ext.unit.junit.VertxUnitRunner;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static io.reactiverse.es4x.test.JS.commonjs;
+
+@RunWith(VertxUnitRunner.class)
+public class StructuredCloneTest {
+
+  private Runtime runtime;
+
+  @Rule
+  public RunTestOnContext rule = new RunTestOnContext();
+
+  @Before
+  public void initialize() {
+    runtime = commonjs(rule.vertx());
+  }
+
+  @Test
+  @Ignore
+  public void test(TestContext should) {
+    final Async test = should.async();
+    // pass the assertion to the engine
+    runtime.put("should", should);
+    runtime.put("test", test);
+
+    runtime.eval("require('./clone/clone_test.js')");
+  }
+}
