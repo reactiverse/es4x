39e2590c6a907fb50a59510abcb4d5e34e267009
Paulo Lopes <pmlopes@gmail.com>
2020-05-28 22:08:45 +0200

    add test for refactoring
    Signed-off-by: Paulo Lopes <pmlopes@gmail.com>

---
+++ es4x/src/test/java/io/reactiverse/es4x/test/FSResolverTest.java
@@ -0,0 +1,60 @@
+package io.reactiverse.es4x.test;
+
+import io.reactiverse.es4x.impl.VertxFileSystem;
+import io.vertx.core.Vertx;
+import io.vertx.core.impl.VertxInternal;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import java.io.File;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import static org.junit.Assert.*;
+
+public class FSResolverTest {
+
+  private static final String $ = File.separator;
+
+  private static Vertx vertx;
+  private static VertxFileSystem fs;
+
+  @BeforeClass
+  public static void beforeAll() {
+    vertx = Vertx.vertx();
+    fs = new VertxFileSystem(vertx);
+  }
+
+  @AfterClass
+  public static void afterAll() {
+    vertx.close();
+  }
+
+  @Test
+  @Ignore
+  public void testResolver() throws URISyntaxException {
+    String cwd = VertxFileSystem.getCWD();
+    String cache = ((VertxInternal) vertx).resolveFile("").getPath() + File.separator;
+
+    // resolve to node modules
+    assertEquals(cwd + $ + "node_modules" + $ + "index.js", fs.parsePath("index.js").toString());
+    // resolve to cwd
+    assertEquals(cwd + $ + "index.js", fs.parsePath("./index.js").toString());
+    // resolve to cwd
+    assertEquals(cwd + $ + ".." + $ + "index.js", fs.parsePath("../index.js").toString());
+    // resolve to root
+    assertEquals($ + "index.js", fs.parsePath("/index.js").toString());
+    // rewrite to cwd
+    assertEquals(cwd + $ + "index.js", fs.parsePath(cache + "index.js").toString());
+    // attempt download
+    assertEquals(cwd + $ + "node_modules" + $ + ".download" + $ + "eedc890765ef80e2b57c447a50f911cd" + $ + "@vertx" + $ + "core@3.9.1" + $ + "options.mjs", fs.parsePath(new URI("https://unpkg.io/@vertx/core@3.9.1/options.mjs")).toString());
+    // attempt from download cache
+    assertEquals(cwd + $ + "node_modules" + $ + ".download" + $ + "eedc890765ef80e2b57c447a50f911cd" + $ + "@vertx" + $ + "core@3.9.1" + $ + "options.mjs", fs.parsePath("./node_modules/.download/eedc890765ef80e2b57c447a50f911cd/@vertx/core@3.9.1/options.mjs").toString());
+    // attempt from download using missing url
+    assertEquals(cwd + $ + "node_modules" + $ + ".download" + $ + "eedc890765ef80e2b57c447a50f911cd" + $ + "@vertx" + $ + "core@3.9.1" + $ + "index.mjs", fs.parsePath("./node_modules/.download/eedc890765ef80e2b57c447a50f911cd/@vertx/core@3.9.1/index.mjs").toString());
+    // resolve to node modules index
+    assertEquals(cwd + $ + "node_modules" + $ + "@vertx" + $ + "web" + $ + "index.mjs", fs.parsePath("@vertx/web").toString());
+  }
+}
