aa6ca5e84979be5b314dc7bc2a61b65a51c5935a
Paulo Lopes <pmlopes@gmail.com>
2020-09-02 11:39:14 +0200

    Rebase

--- pm/src/main/java/io/reactiverse/es4x/cli/Helper.java
+++ pm/src/main/java/io/reactiverse/es4x/cli/Helper.java
@@ -13,7 +13,7 @@
  *
  *  You may elect to redistribute this code under either of these licenses.
  */
-package io.reactiverse.es4x.commands;
+package io.reactiverse.es4x.cli;
 
 import java.io.File;
 import java.io.IOException;
