2f69429a7664773787980c03ef93ec4765268d2e
Paulo Lopes <pmlopes@gmail.com>
2022-01-05 14:09:24 +0100

    Force canonical paths in resolver
    Signed-off-by: Paulo Lopes <pmlopes@gmail.com>

--- es4x/src/main/java/io/reactiverse/es4x/impl/VertxFileSystem.java
+++ es4x/src/main/java/io/reactiverse/es4x/impl/VertxFileSystem.java
@@ -233,6 +233,12 @@ public final class VertxFileSystem implements FileSystem {
   }
 
   private boolean fetchIfNeeded(File file, String path) {
+
+//    downloadDir = D:\a\es4x\es4x\es4x\node_modules\.download\
+//    file        = node_modules\.download\eedc890765ef80e2b57c447a50f911cd\@vertx\web@4.2.0\enums.mjs
+//    path        = D:\a\es4x\es4x\es4x\node_modules\.download\eedc890765ef80e2b57c447a50f911cd\@vertx\web@4.2.0\enums.mjs
+//    target      = eedc890765ef80e2b57c447a50f911cd\@vertx\web@4.2.0\enums.mjs
+
     LOGGER.trace(String.format("fetchIfNeeded(%s, %s)", file, path));
 
     if (path.startsWith(downloadDir)) {
@@ -254,6 +260,11 @@ public final class VertxFileSystem implements FileSystem {
           System.err.println(file);
           System.err.println(path);
           System.err.println(target);
+          System.err.println(source);
+
+          System.err.println(urlMap.get(source));
+          System.err.println(target.substring(split));
+
           throw new InvalidPathException(path, e.getMessage());
         }
       }
